14f0a693f4a75ee26f0e899e659cb8d81a8c1b7b
rrxrs <kadoshrr2019@gmail.com>
2020-12-30 23:58:57 -0500

    Add Added Property Messages Selected

--- src/components/ChatRoom/Sidebar/SidebarChat/SidebarChat.js
+++ src/components/ChatRoom/Sidebar/SidebarChat/SidebarChat.js
@@ -23,7 +23,7 @@ const SidebarChat = ({ photo, userName, conversationId }) => {
       // console.log(conversationId)
       console.log("SidebarChat: getMessages");
       // const baseURL = `https://academlo-whats.herokuapp.com/api/v1/conversations/${conversationId}/messages`;
-      const baseURL = `https://academlo-whats.herokuapp.com/api/v1/conversations/5fe15a225901e80017f682d3/messages`;
+      const baseURL = `https://academlo-whats.herokuapp.com/api/v1/conversations/5fed3094794c290017d822b0/messages`;
       const message = await dispatch(fetchMessages(baseURL, conversationId));
       alert(`SidebarChat: getMessages => ${message}`);
     }catch(error){
--- src/redux/reducers/chatReducer.js
+++ src/redux/reducers/chatReducer.js
@@ -4,13 +4,11 @@ const INITIAL_STATE = {
    conversations: [],
    conversationId: false,
    messages: [
-      { messages: [
-         { userId: false}
-      ] }
+      { _id: false }
    ],
    chatUser: [
       {
-         photoUrl: false,
+         photoUrl: "https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg",
          username: false
       }
    ]
@@ -22,11 +20,27 @@ export const chatReducer = (prevState = INITIAL_STATE, action) => {
 
    switch(action.type) {
       case actions.GET_CONVERSATIONS:
-         return {...transState, conversations: action.payload}; //Conversations in existance
+         const addedPropertyConversations = [];
+         const addedPropertyConversation = {conversationSelected: false};
+         const conversationsObj = action.payload;
+         conversationsObj.map(conversation => {
+            const addingPropertyConversation = Object.assign(conversation, addedPropertyConversation);
+            addedPropertyConversations.push(addingPropertyConversation);
+            return true
+         });
+         return {...transState, conversations: addedPropertyConversations}; //Conversations in existance
       case actions.CONVERSATION_ID:
          return {...transState, conversationId: action.payload}; //Conversation's id
       case actions.GET_MESSAGES:
-         return {...transState, messages: action.payload}; //Messages from a conversation
+         const addedPropertyMessages = [];
+         const addedPropertyMessage = {messageSelected: false};
+         const messagesObj = action.payload;
+         messagesObj[0].messages.forEach(message => {
+            const addingPropertyMessage = Object.assign(message, addedPropertyMessage);
+            addedPropertyMessages.push(addingPropertyMessage);
+         });
+         messagesObj[0].messages = addedPropertyMessages
+         return {...transState, messages: messagesObj}; //Messages from a conversation
       case actions.CHAT_USER:
          return {...transState, chatUser: action.payload}; //User chatting with the userApp
       default:
